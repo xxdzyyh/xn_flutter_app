1c02c5b7058712a9e0b19c93bcfec8799274acdf
饶鹏程 <raopengcheng@xiaoniu66.com>
2019-04-29 17:19:22 +0800

    修改颜色

--- lib/uibuild/xncolor.dart
+++ lib/uibuild/xncolor.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 ///0xFFFF4A00
 const Color xn_orange = Color(0xFFFF4A00);
 
+///0xFF7A00
+const Color xn_orange_2 = Color(0xFFFF7A00);
+
 ///0xFF343434
 const Color xn_black_deep = Color(0xFF343434);
 
@@ -17,5 +20,9 @@ const Color xn_black_dark = Color(0xFFB0B0B0);
 
 ///0xF4F4F4
 const Color xn_bg_color = Color(0xFFF4F4F4);
+
+///0xEEEEEE
+const Color xn_line_color = Color(0xFFEEEEEE);
+
 
 
