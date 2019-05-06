1336570e61e666d027be9af11dc64898881184be
wangxuefeng <wangxuefeng2@xiaoniu66.com>
2019-05-06 18:07:01 +0800

    我的第一步

--- lib/views/my.dart
+++ lib/views/my.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:xn_flutter_app/uibuild/xncolor.dart';
+import 'package:xn_flutter_app/views/my_page/account_vm.dart';
 
 class MyPage extends StatefulWidget {
   @override
@@ -8,6 +9,9 @@ class MyPage extends StatefulWidget {
 }
 
 class _MyPageState extends State<MyPage> {
+
+  var accountVM = XNAccoutVM()
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -15,8 +19,51 @@ class _MyPageState extends State<MyPage> {
         middle: Text("我的"),
       ),
       child: Center(
-        child: Text('哈哈'),
+        child: _setupListView(),
       ),
     );
   }
+
+
+  Widget _setupListView() {
+
+    return ListView.builder(
+      itemCount: 1+this.accountVM.singleItems.length,
+      itemBuilder: (context,i) {
+        if (i == 0) {
+          return _setupHeader();
+        } else {
+          XNAccountSingleCellModel vm = this.accountVM.singleItems[i-1];
+
+          return Text(vm.title);
+        }
+
+      });
+  }
+
+
+  // head
+
+  _setupNotLoginView() {
+
+    Widget welcomeLabel = Text("注册送518元红包",style: TextStyle(fontSize: 14),);
+    Widget tipsLabel = Text("越投入越美好",style: TextStyle(fontSize: 14));
+    Widget loginButton = CupertinoButton(child: Text("登录/注册",style: TextStyle(fontSize: 14)), onPressed: (){
+      print("登录/注册");
+    });
+
+    return Column(
+      children: <Widget>[ welcomeLabel,
+                          tipsLabel ,
+                          loginButton],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+    );
+  }
+
+  _setupHeader() {
+
+    return Column(children: <Widget>[_setupNotLoginView()],mainAxisAlignment: MainAxisAlignment.center,);
+  }
+
 }
